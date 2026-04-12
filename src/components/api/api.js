add9a128ae2f8dfe994a4d2e16816bae8b722c30
JoeTheBest <jewelshahi10@gmail.com>
2026-04-12 22:52:55 +0300

    Changed api.js

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
 const api = axios.create({
-  baseURL: "https://sap-project-backend-2acs.onrender.com/api",
+  baseURL: BASE_URL,
   // baseURL: "http://localhost:5000/api",
 });
 
 export const refreshApi = axios.create({
-  baseURL: "https://sap-project-backend-2acs.onrender.com/api",
+  baseURL: BASE_URL,
   // baseURL: "http://localhost:5000/api",
 });
 
